065885f12988ff45a86b2ee8d8a846c7440fed22
Trung <hvthn126@gmail.com>
2023-11-11 06:31:45 +0700

    chat java socket

---
+++ src/sendfile/client/MainForm.java
@@ -0,0 +1,448 @@
+package sendfile.client;
+
+
+import sendfile.client.LoginForm;
+import sendfile.client.ClientThread;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Vector;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class MainForm extends javax.swing.JFrame {
+    String username;
+    String host;
+    int port;
+    Socket socket;
+    DataOutputStream dos;
+    public boolean attachmentOpen = false;
+    private boolean isConnected = false;
+    private String mydownloadfolder = "D:\\";
+
+    /**
+     * Creates new form MainForm
+     */
+    public MainForm() {
+        initComponents();
+        MyInit();
+    }
+        public void initFrame(String username, String host, int port){
+        this.username = username;
+        this.host = host;
+        this.port = port;
+        setTitle("Bạn đang được đăng nhập với tên: " + username);
+        //Kết nối 
+        connect();
+    }
+     void MyInit(){
+         setLocationRelativeTo(null);
+     }
+    
+    public void connect(){
+        appendMessage(" Đang kết nối...", "Trạng thái", Color.BLUE, Color.BLUE);
+        try {
+            socket = new Socket(host, port);
+            dos = new DataOutputStream(socket.getOutputStream());
+            // gửi username đang kết nối
+            dos.writeUTF("CMD_JOIN "+ username);
+            appendMessage(" Đã kết nối", "Trạng thái", Color.BLUE, Color.BLUE);
+            appendMessage(" Gửi tin nhắn bây giờ!", "Trạng thái", Color.BLUE, Color.BLUE);
+            
+            // Khởi động Client Thread 
+            new Thread(new ClientThread(socket, this)).start();
+            jButton1.setEnabled(true);
+            // đã được kết nối
+            isConnected = true;
+            
+        }
+        catch(IOException e) {
+            isConnected = false;
+            JOptionPane.showMessageDialog(this, "Không thể kết nối đến máy chủ, vui lòng thử lại sau.!","Kết nối thất bại",JOptionPane.ERROR_MESSAGE);
+            appendMessage("[IOException]: "+ e.getMessage(), "Lỗi", Color.RED, Color.RED);
+        }
+    }
+    
+    /*
+        Được kết nối
+    */
+    public boolean isConnected(){
+        return this.isConnected;
+    }
+    
+    /*
+        Hiển thị Message
+    */
+    public void appendMessage(String msg, String header, Color headerColor, Color contentColor){
+        jTextPane1.setEditable(true);
+        getMsgHeader(header, headerColor);
+        getMsgContent(msg, contentColor);
+        jTextPane1.setEditable(false);
+    }
+    
+    /*
+        Tin nhắn chat
+    */
+    public void appendMyMessage(String msg, String header){
+        jTextPane1.setEditable(true);
+        getMsgHeader(header, Color.GREEN);
+        getMsgContent(msg, Color.BLACK);
+        jTextPane1.setEditable(false);
+    }
+    
+    /*
+        Tiêu đề tin nhắn
+    */
+    public void getMsgHeader(String header, Color color){
+        int len = jTextPane1.getDocument().getLength();
+        jTextPane1.setCaretPosition(len);
+        jTextPane1.setCharacterAttributes(MessageStyle.styleMessageContent(color, "Arial", 13), false);
+        jTextPane1.replaceSelection(header+":");
+    }
+    /*
+        Nội dung tin nhắn
+    */
+    public void getMsgContent(String msg, Color color){
+        int len = jTextPane1.getDocument().getLength();
+        jTextPane1.setCaretPosition(len);
+        jTextPane1.setCharacterAttributes(MessageStyle.styleMessageContent(color, "Arial", 12), false);
+        jTextPane1.replaceSelection(msg +"\n\n");
+    }
+    
+//    public void appendOnlineList(Vector list){
+//        sampleOnlineList(list); 
+//    }
+    
+    /*
+        Hiển thị danh sách đang online
+    */
+//    public void showOnLineList(Vector list){
+//        try {
+//            txtpane2.setEditable(true);
+//            txtpane2.setContentType("text/html");
+//            StringBuilder sb = new StringBuilder();
+//            Iterator it = list.iterator();
+//            sb.append("<html><table>");
+//            while(it.hasNext()){
+//                Object e = it.next();
+//                URL url = getImageFile();
+//                Icon icon = new ImageIcon(this.getClass().getResource("/images/online.png"));
+//                sb.append("<tr><td><b>></b></td><td>").append(e).append("</td></tr>");
+//                System.out.println("Online: "+ e);
+//            }
+//            sb.append("</table></body></html>");
+//            txtpane2.removeAll();
+//            txtpane2.setText(sb.toString());
+//            txtpane2.setEditable(false);
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
+    
+    /*
+      ************************************  Hiển thị danh sách online  *********************************************
+    */
+//    private void sampleOnlineList(Vector list){
+//        txtpane2.setEditable(true);
+//        txtpane2.removeAll();
+//        txtpane2.setText("");
+//        Iterator i = list.iterator();
+//        while(i.hasNext()){
+//            Object e = i.next();
+//            /*  Hiển thị Username Online   */
+//            JPanel panel = new JPanel();
+//            panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+//            panel.setBackground(Color.white);
+//            
+//            Icon icon = new ImageIcon(this.getClass().getResource("/images/online.png"));
+//            JLabel label = new JLabel(icon);
+//            label.setText(" "+ e);
+//            panel.add(label);
+//            int len = txtpane2.getDocument().getLength();
+//            txtpane2.setCaretPosition(len);
+//            txtpane2.insertComponent(panel);
+//            /*  Append Next Line   */
+//            sampleAppend();
+//        }
+//        txtpane2.setEditable(false);
+//    }
+//    private void sampleAppend(){
+//        int len = txtpane2.getDocument().getLength();
+//        txtpane2.setCaretPosition(len);
+//        txtpane2.replaceSelection("\n");
+//    }
+    /*
+      ************************************  Show Online Sample  *********************************************
+    */
+    
+    
+    
+    
+    /*
+        Get image file path
+    */
+    public URL getImageFile(){
+        URL url = this.getClass().getResource("/images/online.png");
+        return url;
+    }
+    
+    
+    /*
+        Set myTitle
+    */
+    public void setMyTitle(String s){
+        setTitle(s);
+    }
+    
+    /*
+        Phương thức tải get download
+    */
+    public String getMyDownloadFolder(){
+        return this.mydownloadfolder;
+    }
+    
+    /*
+        Phương thức get host
+    */
+    public String getMyHost(){
+        return this.host;
+    }
+    
+    /*
+        Phương thức get Port
+    */
+    public int getMyPort(){
+        return this.port;
+    }
+    
+    /*
+        Phương thức nhận My Username
+    */
+    public String getMyUsername(){
+        return this.username;
+    }
+    
+    /*
+        Cập nhật Attachment 
+    */
+    public void updateAttachment(boolean b){
+        this.attachmentOpen = b;
+    }
+    
+    /*
+        Hàm này sẽ mở 1 file chooser
+    */
+    public void openFolder(){
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int open = chooser.showDialog(this, "Mở Thư Mục");
+        if(open == chooser.APPROVE_OPTION){
+            mydownloadfolder = chooser.getSelectedFile().toString()+"\\";
+        } else {
+            mydownloadfolder = "D:\\";
+        }
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextPane1 = new javax.swing.JTextPane();
+        jTextField1 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        sendFileMenu = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(204, 255, 255));
+
+        jTextPane1.setFont(new java.awt.Font("Yu Gothic UI Light", 0, 11)); // NOI18N
+        jScrollPane1.setViewportView(jTextPane1);
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jButton1.setBackground(new java.awt.Color(255, 153, 153));
+        jButton1.setText("Gửi");
+        jButton1.setEnabled(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jMenu2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/check.png"))); // NOI18N
+        jMenu2.setText("Tài Khoản");
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/sharing.png"))); // NOI18N
+        jMenu3.setText("Chia Sẻ File");
+        jMenu3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenu3ActionPerformed(evt);
+            }
+        });
+
+        sendFileMenu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/sendfile.png"))); // NOI18N
+        sendFileMenu.setText("Gửi File");
+        sendFileMenu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sendFileMenuActionPerformed(evt);
+            }
+        });
+        jMenu3.add(sendFileMenu);
+
+        jMenuBar1.add(jMenu3);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 437, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(178, 178, 178)
+                                .addComponent(jLabel3))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(12, 12, 12)
+                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jLabel3))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 353, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(6, 6, 6))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenu3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenu3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenu3ActionPerformed
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+        try {
+            String content = username+" "+ evt.getActionCommand();
+            dos.writeUTF("CMD_CHATALL "+ content);
+            appendMyMessage(" "+evt.getActionCommand(), username);
+            jTextField1.setText("");
+        } catch (IOException e) {
+            appendMessage(" Không thể gửi tin nhắn đi bây giờ, không thể kết nối đến Máy Chủ tại thời điểm này, xin vui lòng thử lại sau hoặc khởi động lại ứng dụng này.!", "Lỗi", Color.RED, Color.RED);
+        }
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        try {
+            String content = username+" "+ jTextField1.getText();
+            dos.writeUTF("CMD_CHATALL "+ content);
+            appendMyMessage(" "+jTextField1.getText(), username);
+            jTextField1.setText("");
+        } catch (IOException e) {
+            appendMessage(" Không thể gửi tin nhắn đi bây giờ, không thể kết nối đến Máy Chủ tại thời điểm này, xin vui lòng thử lại sau hoặc khởi động lại ứng dụng này.!", "Lỗi", Color.RED, Color.RED);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void sendFileMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendFileMenuActionPerformed
+        // TODO add your handling code here:
+        if(!attachmentOpen){
+            SendFile s = new SendFile();
+            if(s.prepare(username, host, port, this)){
+                s.setLocationRelativeTo(null);
+                s.setVisible(true);
+                attachmentOpen = true;
+            } else {
+                JOptionPane.showMessageDialog(this, "Không thể thiết lập Chia sẻ File tại thời điểm này, xin vui lòng thử lại sau.!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }//GEN-LAST:event_sendFileMenuActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainForm().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextPane jTextPane1;
+    private javax.swing.JMenuItem sendFileMenu;
+    // End of variables declaration//GEN-END:variables
+}
